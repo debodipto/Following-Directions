390bff038531ad52fe1729b1443cf142efef691e
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-03-03 20:26:51 +0600

    Update Following Directions-codeforces.cpp

--- Following Directions-codeforces.cpp
+++ Following Directions-codeforces.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+bool check(string s, int n)
+{
+    int x=0,y=0;
+    for(int i=0;i<n;i++)
+    {
+        if(s[i]=='U')
+        y++;
+        if(s[i]=='D')
+        y--;
+        if(s[i]=='R')
+        x++;
+        if(s[i]=='L')
+        x--;
+        if(x==1 && y==1)
+        return 1;
+    }
+    return 0;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        cout<<((check(s,n))?"YES":"NO")<<endl;
+    }
+    return 0;
+}
